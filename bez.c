3f409c9431d037110aaf18b543c1abbc2c923d13
LogicEu <eugenioarteagaa@gmail.com>
2023-10-21 01:20:51 -0300

    added spline structure and implemented some common bezier control functionality

--- bez.c
+++ bez.c
@@ -11,7 +11,17 @@
 #include <stdio.h>
 #include <string.h>
 
-void pxPlotBezierCubic(const Tex2D texture, vec2 a, vec2 b, vec2 c, vec2 d, const Px col)
+struct Spline {
+    vec2 p, c[2];
+};
+
+static struct Spline pxSplineCreate(vec2 p)
+{
+    return (struct Spline) {p, {p, p}};
+}
+
+static void pxPlotBezierCubic(
+    const Tex2D texture, vec2 a, vec2 b, vec2 c, vec2 d, const Px col)
 {
     float t;
     vec2 q, p = a;
@@ -48,12 +58,13 @@ int main(const int argc, const char** argv)
     }
 
     const Px red = {255, 0, 0, 255}, green = {0, 255, 0, 255}, blue = {0, 0, 255, 255};
-    struct vector point_vector = vector_create(sizeof(vec2));
-    Px* fb = spxeStart("bezier", 800, 600, width, height);
+    struct vector splinevec = vector_create(sizeof(struct Spline));
+    Px* fb = spxeStart("bez", 800, 600, width, height);
     Tex2D texture = {fb, width, height};
     const size_t size = width * height * sizeof(Px);
-    int isinside = 0, clicked = 0, down = 0;
-    size_t i, count, selected = 0, overlay = 1;
+    int isinside = 0, clicked = 0, down = 0, overlay = 1, isclosed = 0;
+    int justcreated = 0;
+    size_t i, count, selected = 0;
     ivec2 mouse = {0};
     vec2 m;
 
@@ -61,9 +72,11 @@ int main(const int argc, const char** argv)
         if (spxeKeyPressed(ESCAPE)) {
             break;
         }
-        
         if (spxeKeyPressed(R)) {
-            vector_clear(&point_vector);
+            vector_clear(&splinevec);
+        }
+        if (spxeKeyPressed(X) || spxeKeyPressed(BACKSPACE)) {
+            vector_pop(&splinevec);
         }
         if (spxeKeyPressed(Q)) {
             overlay = !overlay;
@@ -75,45 +88,82 @@ int main(const int argc, const char** argv)
         down = spxeMouseDown(LEFT);
         isinside = mouse.x >= 0 && mouse.x < width && mouse.y >= 0 && mouse.y < height;
         memset(texture.pixbuf, 155, size);
+        
+        count = splinevec.size;
+        vec2* points = splinevec.data;
+        struct Spline *splines = splinevec.data;
 
-        count = point_vector.size;
-        vec2* points = point_vector.data;
-        for (i = 0; i + 4 <= count; i += 3) {
+        if (isclosed) {
             pxPlotBezierCubic(
-                texture, points[i], points[i + 1], points[i + 2], points[i + 3], blue
+                texture, splines[count - 1].p,
+                splines[count - 1].c[1], splines[0].c[0], splines[0].p, blue
+            );
+        }
+
+        for (i = 0; i + 1 < count; ++i) {
+            pxPlotBezierCubic(
+                texture, splines[i].p, 
+                splines[i].c[1], splines[i + 1].c[0], splines[i + 1].p, blue
             );
 
             if (overlay) {
-                pxPlotLineSmooth(texture, points[i], points[i + 1], red);
-                pxPlotLineSmooth(texture, points[i + 1], points[i + 2], red);
-                pxPlotLineSmooth(texture, points[i + 2], points[i + 3], red);
+                pxPlotLineSmooth(texture, splines[i].p, splines[i].c[0], red);
+                pxPlotLineSmooth(texture, splines[i].p, splines[i].c[1], red);
             }
         }
 
-        for (i = 0; i < count; ++i) {
-            pxPlot(texture, (int)points[i].x, (int)points[i].y, green);
-            if (!selected && down && m.x == points[i].x && m.y == points[i].y) {
-                selected = i + 1;
+        if (count && overlay) {
+            pxPlotLineSmooth(texture, splines[i].p, splines[i].c[0], red);
+            pxPlotLineSmooth(texture, splines[i].p, splines[i].c[1], red);
+            count = splinevec.size * 3;
+            for (i = 0; i < count; ++i) {
+                pxPlot(texture, (int)points[i].x, (int)points[i].y, green);
+                if (!selected && down && m.x == points[i].x && m.y == points[i].y) {
+                    selected = i + 1;
+                }
             }
         }
-        
+
         if (spxeMouseReleased(LEFT)) {
             selected = 0;
+            justcreated = 0;
         }
 
         if (isinside) {
-            if (selected) {
-                points[selected - 1] = m;
+            if (!isclosed && selected == 1 && splinevec.size > 2) {
+               isclosed = 1;
+            } else if (selected) {
+                const size_t index = selected - 1;
+                if (justcreated) {
+                    vec2 d = vec2_sub(m, points[index]);
+                    points[index + 1] = vec2_sub(points[index], d);
+                    points[index + 2] = m;
+                } else if (index % 3 == 0) {
+                    vec2 d = vec2_sub(m, points[index]);
+                    points[index] = m;
+                    points[index + 1].x += d.x;
+                    points[index + 1].y += d.y;
+                    points[index + 2].x += d.x;
+                    points[index + 2].y += d.y;
+                } else {
+                    const int couple = (index + 1) % 3 == 0 ? -1 : 1;
+                    const int anchor = couple == 1 ? -1 : -2;
+                    vec2 d = vec2_sub(points[index], points[index + anchor]);
+                    points[index] = m;
+                    points[index + couple] = vec2_sub(points[index + anchor], d);
+                }
             } else {
                 pxPlot(texture, mouse.x, mouse.y, red);
                 if (clicked) {
-                    vector_push(&point_vector, &m);
+                    struct Spline s = pxSplineCreate(m);
+                    vector_push(&splinevec, &s);
+                    justcreated = 1;
                 }
             }
         }
     }
 
-    vector_free(&point_vector);
+    vector_free(&splinevec);
     return spxeEnd(fb);
 }
 
